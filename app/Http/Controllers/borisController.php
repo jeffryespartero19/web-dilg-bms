3b369c8a499a22ce407df98528eeeecb8e22a6f4
Ralf <ralf.pontilar@yahoo.com>
2022-08-09 15:41:01 +0800

    Update BORIS Ordinance and Resolution

--- app/Http/Controllers/borisController.php
+++ app/Http/Controllers/borisController.php
@@ -3,79 +3,134 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\User;
+use Carbon\Carbon;
+use DB;
 
 class borisController extends Controller
 {
     //Inhabitants Information List
-    public function inhabitants_information_list(Request $request)
+    public function ordinances_and_resolutions_list(Request $request)
     {
         $currDATE = Carbon::now();
         $db_entries = DB::table('boris_brgy_ordinances_and_resolutions_information as a')
-            ->leftjoin('maintenance_bips_name_prefix as b', 'a.Name_Prefix_ID', '=', 'b.Name_Prefix_ID')
-            ->leftjoin('maintenance_bips_name_suffix as c', 'a.Name_Suffix_ID', '=', 'c.Name_Suffix_ID')
+            ->leftjoin('maintenance_boris_status_of_ordinance_or_resolution as b', 'a.Status_of_Ordinance_or_Resolution_ID', '=', 'b.Status_of_Ordinance_or_Resolution_ID')
             ->select(
-                'a.Resident_ID',
-                'a.Name_Prefix_ID',
-                'a.Last_Name',
-                'a.First_Name',
-                'a.Middle_Name',
-                'a.Name_Suffix_ID',
-                'a.Birthplace',
-                'a.Weight',
-                'a.Height',
-                'a.Civil_Status_ID',
-                'a.Birthdate',
-                'a.Country_ID',
-                'a.Religion_ID',
-                'a.Blood_Type_ID',
-                'a.Sex',
-                'a.Mobile_No',
-                'a.Telephone_No',
-                'a.Barangay_ID',
-                'a.City_Municipality_ID',
-                'a.Province_ID',
-                'a.Region_ID',
-                'a.Salary',
-                'a.Email_Address',
-                'a.PhilSys_Card_No',
-                'a.Solo_Parent',
-                'a.OFW',
-                'a.Indigent',
-                'a.4Ps_Beneficiary',
-                'a.Encoder_ID',
-                'a.Date_Stamp',
-                'b.Name_Prefix',
-                'c.Name_Suffix'
+                'a.Ordinance_Resolution_ID',
+                'a.Ordinance_or_Resolution',
+                'a.Ordinance_Resolution_No',
+                'a.Date_of_Approval',
+                'a.Date_of_Effectivity',
+                'a.Ordinance_Resolution_Title',
+                'a.Status_of_Ordinance_or_Resolution_ID',
+                'b.Name_of_Status'
+
             )
             ->paginate(20, ['*'], 'db_entries');
-        $religion = DB::table('maintenance_bips_religion')->where('Active', 1)->get();
-        $blood_type = DB::table('maintenance_bips_blood_type')->where('Active', 1)->get();
-        $civil_status = DB::table('maintenance_bips_civil_status')->where('Active', 1)->get();
-        $prefix = DB::table('maintenance_bips_name_prefix')->where('Active', 1)->get();
-        $suffix = DB::table('maintenance_bips_name_suffix')->where('Active', 1)->get();
+
         $region = DB::table('maintenance_region')->where('Active', 1)->get();
         $province = DB::table('maintenance_province')->where('Active', 1)->get();
         $city = DB::table('maintenance_city_municipality')->where('Active', 1)->get();
         $barangay = DB::table('maintenance_barangay')->where('Active', 1)->get();
-        $country = DB::table('maintenance_country')->where('Active', 1)->get();
-        $academic_level = DB::table('maintenance_bips_academic_level')->where('Active', 1)->get();
-        $employment_type = DB::table('maintenance_bips_employment_type')->where('Active', 1)->get();
+        $status = DB::table('maintenance_boris_status_of_ordinance_or_resolution')->where('Active', 1)->get();
+        $type = DB::table('maintenance_boris_type_of_ordinance_or_resolution')->where('Active', 1)->get();
+        $category = DB::table('maintenance_boris_category_of_ordinance_or_resolution_id')->where('Active', 1)->get();
 
-        return view('bips_transactions.inhabitants_information_list', compact(
+        return view('boris_transactions.ordinances_and_resolutions_list', compact(
             'db_entries',
             'currDATE',
-            'religion',
-            'blood_type',
-            'civil_status',
-            'prefix',
-            'suffix',
             'region',
             'province',
             'city',
             'barangay',
-            'country',
-            'academic_level',
-            'employment_type'
+            'status',
+            'type',
+            'category',
         ));
+    }
+
+    // Save Inhabitants Household Info
+    public function create_ordinance_and_resolution(Request $request)
+    {
+        $currDATE = Carbon::now();
+        $data = $data = request()->all();
+
+        if ($data['Ordinance_Resolution_ID'] == null || $data['Ordinance_Resolution_ID'] == 0) {
+            DB::table('boris_brgy_ordinances_and_resolutions_information')->insert(
+                array(
+                    'Ordinance_or_Resolution' => (int)$data['Ordinance_or_Resolution'],
+                    'Ordinance_Resolution_No' => $data['Ordinance_Resolution_No'],
+                    'Date_of_Approval' => $data['Date_of_Approval'],
+                    'Date_of_Effectivity' => $data['Date_of_Effectivity'],
+                    'Ordinance_Resolution_Title' => $data['Ordinance_Resolution_Title'],
+                    'Status_of_Ordinance_or_Resolution_ID' => $data['Status_of_Ordinance_or_Resolution_ID'],
+                    'Abstract_Content' => $data['Abstract_Content'],
+                    'Previous_Related_Ordinance_Resolution_ID' => $data['Previous_Related_Ordinance_Resolution_ID'],
+                    'Barangay_ID' => $data['Barangay_ID'],
+                    'City_Municipality_ID' => $data['City_Municipality_ID'],
+                    'Province_ID' => $data['Province_ID'],
+                    'Region_ID' => $data['Region_ID'],
+                    'Encoder_ID'       => Auth::user()->id,
+                    'Date_Stamp'       => Carbon::now()
+                )
+            );
+
+            return redirect()->back()->with('message', 'New Record Created');
+        } else {
+            DB::table('boris_brgy_ordinances_and_resolutions_information')->where('Ordinance_Resolution_ID', $data['Ordinance_Resolution_ID'])->update(
+                array(
+                    'Ordinance_or_Resolution' => (int)$data['Ordinance_or_Resolution'],
+                    'Ordinance_Resolution_No' => $data['Ordinance_Resolution_No'],
+                    'Date_of_Approval' => $data['Date_of_Approval'],
+                    'Date_of_Effectivity' => $data['Date_of_Effectivity'],
+                    'Ordinance_Resolution_Title' => $data['Ordinance_Resolution_Title'],
+                    'Status_of_Ordinance_or_Resolution_ID' => $data['Status_of_Ordinance_or_Resolution_ID'],
+                    'Abstract_Content' => $data['Abstract_Content'],
+                    'Previous_Related_Ordinance_Resolution_ID' => $data['Previous_Related_Ordinance_Resolution_ID'],
+                    'Barangay_ID' => $data['Barangay_ID'],
+                    'City_Municipality_ID' => $data['City_Municipality_ID'],
+                    'Province_ID' => $data['Province_ID'],
+                    'Region_ID' => $data['Region_ID'],
+                    'Encoder_ID'       => Auth::user()->id,
+                    'Date_Stamp'       => Carbon::now()
+                )
+            );
+
+            return redirect()->back()->with('message', 'Record Updated');
+        }
+    }
+
+    // Display Household Details
+    public function get_ordinance_and_resolution_info(Request $request)
+    {
+        $id = $_GET['id'];
+
+        $theEntry = DB::table('boris_brgy_ordinances_and_resolutions_information as a')
+            ->leftjoin('maintenance_barangay as b', 'a.Barangay_ID', '=', 'b.Barangay_ID')
+            ->leftjoin('maintenance_city_municipality as c', 'a.City_Municipality_ID', '=', 'c.City_Municipality_ID')
+            ->leftjoin('maintenance_province as d', 'a.Province_ID', '=', 'd.Province_ID')
+            ->select(
+                'a.Ordinance_Resolution_ID',
+                'a.Ordinance_or_Resolution',
+                'a.Ordinance_Resolution_No',
+                'a.Date_of_Approval',
+                'a.Date_of_Effectivity',
+                'a.Ordinance_Resolution_Title',
+                'a.Status_of_Ordinance_or_Resolution_ID',
+                'a.Abstract_Content',
+                'a.Previous_Related_Ordinance_Resolution_ID',
+                'a.Barangay_ID',
+                'a.City_Municipality_ID',
+                'a.Province_ID',
+                'a.Region_ID',
+                'b.Barangay_Name',
+                'c.City_Municipality_Name',
+                'd.Province_Name',
+            )
+            ->where('a.Ordinance_Resolution_ID', $id)
+            ->get();
+
+        return (compact('theEntry'));
     }
 }
---
+++ resources/views/boris_transactions/ordinances_and_resolutions_list.blade.php
@@ -0,0 +1,356 @@
+@extends('layouts.default')
+
+@section('content')
+<script src="{{ asset('/js/maintenance.js') }}" defer></script>
+<link href="{{ asset('/css/maintenance.css') }}" rel="stylesheet">
+
+<div class="page_title_row col-md-12">
+    <div class="col-md-6 titleXZ"> Ordinances and Resolutions List </div>
+    <div class="col-md-6 breadcrumbXZ">
+        <ol class="breadcrumb">
+            <a href="{{route('home')}}">
+                <li>DILG_BMS / </li>
+            </a>
+            <li> &nbsp;Ordinances and Resolutions List</li>
+        </ol>
+    </div>
+</div>
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+@if(session()->has('message'))
+<div class="alert alert-success">
+    {{ session()->get('message') }}
+</div>
+@endif
+<div class="tableX_row col-md-12 up_marg5">
+    <br>
+    <div class="flexer">
+        <div class="eighty_split">{{$db_entries->appends(['db_entries' => $db_entries->currentPage()])->links()}}</div>
+        <div class="twenty_split txtRight"><button data-toggle="modal" class="btn btn-success" data-target="#createOrdinance_Info" style="width: 100px;">New</button></div>
+    </div>
+    <br>
+    <div class="col-md-12">
+        <table id="example" class="table table-striped table-bordered" style="width:100%">
+            <thead>
+                <tr>
+                    <th hidden>Ordinance_Resolution_ID</th>
+                    <th>Ordinance_or_Resolution</th>
+                    <th>Ordinance_Resolution_No</th>
+                    <th>Date_of_Approval</th>
+                    <th>Date_of_Effectivity</th>
+                    <th>Ordinance_Resolution_Title</th>
+                    <th>Status_of_Ordinance_or_Resolution_ID</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($db_entries as $x)
+                <tr>
+                    <td class="sm_data_col txtCtr" hidden>{{$x->Ordinance_Resolution_ID}}</td>
+                    <td class="sm_data_col txtCtr">@if($x->Ordinance_or_Resolution == 1) Resolution @else Ordinance @endif</td>
+                    <td class="sm_data_col txtCtr">{{$x->Ordinance_Resolution_No}}</td>
+                    <td class="sm_data_col txtCtr">{{$x->Date_of_Approval}}</td>
+                    <td class="sm_data_col txtCtr">{{$x->Date_of_Effectivity}}</td>
+                    <td class="sm_data_col txtCtr">{{$x->Ordinance_Resolution_Title}}</td>
+                    <td class="sm_data_col txtCtr">{{$x->Name_of_Status}}</td>
+                    <td class="sm_data_col txtCtr">
+                        <button class="edit_ordinance" value="{{$x->Ordinance_Resolution_ID}}" data-toggle="modal" data-target="#createOrdinance_Info">Edit</button>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<!-- Create Announcement_Status Modal -->
+
+<div class="modal fade" id="createOrdinance_Info" tabindex="-1" role="dialog" aria-labelledby="Create_Ordinance" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close modal-close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title flexer justifier" id="Modal_Title">Create Ordinance and Resolution</h4>
+            </div>
+            <div class="modal-body">
+                <form id="newOrdinance" method="POST" action="{{ route('create_ordinance_and_resolution') }}" autocomplete="off" enctype="multipart/form-data">@csrf
+                    <div class="modal-body">
+                        <h3>Name</h3>
+                        <div class="row">
+                            <input type="text" class="form-control" id="Ordinance_Resolution_ID" name="Ordinance_Resolution_ID" value="" hidden>
+                            <div class="form-group col-lg-6" style="padding:0 10px">
+                                <label for="exampleInputEmail1">Type</label>
+                                <select class="form-control" id="Ordinance_or_Resolution" name="Ordinance_or_Resolution" required>
+                                    <option value='' disabled selected>Select Option</option>
+                                    <option value=0>Ordinace</option>
+                                    <option value=1>Resolution</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-lg-6" style="padding:0 10px">
+                                <label for="exampleInputEmail1">Ordinance Resolution No</label>
+                                <input type="text" class="form-control" id="Ordinance_Resolution_No" name="Ordinance_Resolution_No" required>
+                            </div>
+                            <div class="form-group col-lg-6" style="padding:0 10px">
+                                <label for="Date_of_Approval">Date of Approval</label>
+                                <input type="date" class="form-control" id="Date_of_Approval" name="Date_of_Approval" required>
+                            </div>
+                            <div class="form-group col-lg-6" style="padding:0 10px">
+                                <label for="Date_of_Effectivity">Date of Effectivity</label>
+                                <input type="date" class="form-control" id="Date_of_Effectivity" name="Date_of_Effectivity" required>
+                            </div>
+                            <div class="form-group col-lg-6" style="padding:0 10px">
+                                <label for="Ordinance_Resolution_Title">Ordinance Resolution Title</label>
+                                <input type="text" class="form-control" id="Ordinance_Resolution_Title" name="Ordinance_Resolution_Title" required>
+                            </div>
+                            <div class="form-group col-lg-6" style="padding:0 10px">
+                                <label for="Status_of_Ordinance_or_Resolution_ID">Status</label>
+                                <select class="form-control" id="Status_of_Ordinance_or_Resolution_ID" name="Status_of_Ordinance_or_Resolution_ID" required>
+                                    <option value='' disabled selected>Select Option</option>
+                                    @foreach($status as $status)
+                                    <option value="{{ $status->Status_of_Ordinance_or_Resolution_ID  }}">{{ $status->Name_of_Status }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group col-lg-6" style="padding:0 10px">
+                                <label for="Abstract_Content">Abstract Content</label>
+                                <input type="text" class="form-control" id="Abstract_Content" name="Abstract_Content" required>
+                            </div>
+                            <div class="form-group col-lg-6" style="padding:0 10px">
+                                <label for="Previous_Related_Ordinance_Resolution_ID">Previous Related Ordinance Resolution</label>
+                                <select class="form-control" id="Previous_Related_Ordinance_Resolution_ID" name="Previous_Related_Ordinance_Resolution_ID">
+                                    <option value='' selected>Select Option</option>
+                                    @foreach($db_entries as $de)
+                                    <option value="{{ $de->Ordinance_Resolution_ID   }}">{{ $de->Ordinance_Resolution_Title }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group col-lg-6" style="padding:0 10px">
+                                <label for="exampleInputEmail1">Region</label>
+                                <select class="form-control" id="Region_ID" name="Region_ID" required>
+                                    <option value='' disabled selected>Select Option</option>
+                                    @foreach($region as $region)
+                                    <option value="{{ $region->Region_ID }}">{{ $region->Region_Name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group col-lg-6" style="padding:0 10px">
+                                <label for="exampleInputEmail1">Province</label>
+                                <select class="form-control" id="Province_ID" name="Province_ID" required>
+                                    <option value='' disabled selected>Select Option</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-lg-6" style="padding:0 10px">
+                                <label for="City_Municipality_ID">City/Municipality</label>
+                                <select class="form-control" id="City_Municipality_ID" name="City_Municipality_ID" required>
+                                    <option value='' disabled selected>Select Option</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-lg-6" style="padding:0 10px">
+                                <label for="Barangay_ID">Barangay</label>
+                                <select class="form-control" id="Barangay_ID" name="Barangay_ID" required>
+                                    <option value='' disabled selected>Select Option</option>
+                                </select>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger modal-close" style="width: 200px;" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary" style="width: 200px;">Create</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+<!-- Create Announcement_Status END -->
+
+@endsection
+
+@section('scripts')
+
+<script>
+    // Populate Province
+    $(document).on("change", "#Region_ID", function() {
+        // alert('test');
+        var Region_ID = $(this).val();
+
+        $.ajax({
+            type: "GET",
+            url: "/get_province/" + Region_ID,
+            fail: function() {
+                alert("request failed");
+            },
+            success: function(data) {
+                var data = JSON.parse(data);
+                $('#Province_ID').empty();
+                $('#City_Municipality_ID').empty();
+                $('#Barangay_ID').empty();
+
+
+                var option1 =
+                    "<option value='' disabled selected>Select Option</option>";
+                $('#Province_ID').append(option1);
+                $('#City_Municipality_ID').append(option1);
+                $('#Barangay_ID').append(option1);
+
+                data.forEach(element => {
+                    var option = " <option value='" +
+                        element["Province_ID"] +
+                        "'>" +
+                        element["Province_Name"] +
+                        "</option>";
+                    $('#Province_ID').append(option);
+                });
+            }
+        });
+    });
+
+    // Populate City
+    $(document).on("change", "#Province_ID", function() {
+        var Province_ID = $(this).val();
+
+        $.ajax({
+            type: "GET",
+            url: "/get_city/" + Province_ID,
+            fail: function() {
+                alert("request failed");
+            },
+            success: function(data) {
+                var data = JSON.parse(data);
+                $('#City_Municipality_ID').empty();
+                $('#Barangay_ID').empty();
+
+                var option1 =
+                    " <option value='' disabled selected>Select Option</option>";
+                $('#City_Municipality_ID').append(option1);
+                $('#Barangay_ID').append(option1);
+
+                data.forEach(element => {
+                    var option = " <option value='" +
+                        element["City_Municipality_ID"] +
+                        "'>" +
+                        element["City_Municipality_Name"] +
+                        "</option>";
+                    $('#City_Municipality_ID').append(option);
+                });
+            }
+        });
+    });
+
+
+    // Populate Barangay
+    $(document).on("change", "#City_Municipality_ID", function() {
+        var City_Municipality_ID = $(this).val();
+
+        $.ajax({
+            type: "GET",
+            url: "/get_barangay/" + City_Municipality_ID,
+            fail: function() {
+                alert("request failed");
+            },
+            success: function(data) {
+                var data = JSON.parse(data);
+                $('#Barangay_ID').empty();
+
+                var option1 =
+                    " <option value='' disabled selected>Select Option</option>";
+                $('#Barangay_ID').append(option1);
+
+                data.forEach(element => {
+                    var option = " <option value='" +
+                        element["Barangay_ID"] +
+                        "'>" +
+                        element["Barangay_Name"] +
+                        "</option>";
+                    $('#Barangay_ID').append(option);
+                });
+            }
+        });
+    });
+
+    // Data Table
+    $(document).ready(function() {
+        $('#example').DataTable();
+    });
+
+    $(document).on('click', '.modal-close', function(e) {
+        $('#newOrdinance').trigger("reset");
+        $('#Barangay_ID').empty();
+        $('#City_Municipality_ID').empty();
+        $('#Province_ID').empty();
+        var option1 = "<option value='' disabled selected>Select Option</option>";
+        $('#Barangay_ID').append(option1);
+        $('#City_Municipality_ID').append(option1);
+        $('#Province_ID').append(option1);
+        $('#Modal_Title').text('Create Ordinance');
+
+    });
+
+    // Edit Button Display Modal
+    $(document).on('click', ('.edit_ordinance'), function(e) {
+
+        var disID = $(this).val();
+        $('#Modal_Title').text('Edit Ordinance Information');
+        $.ajax({
+            url: "/get_ordinance_and_resolution_info",
+            type: 'GET',
+            data: {
+                id: disID
+            },
+            fail: function() {
+                alert('request failed');
+            },
+            success: function(data) {
+                $('#Ordinance_Resolution_ID').val(data['theEntry'][0]['Ordinance_Resolution_ID']);
+                $('#Ordinance_or_Resolution').val(data['theEntry'][0]['Ordinance_or_Resolution']);
+                $('#Ordinance_Resolution_No').val(data['theEntry'][0]['Ordinance_Resolution_No']);
+                $('#Date_of_Approval').val(data['theEntry'][0]['Date_of_Approval']);
+                $('#Date_of_Effectivity').val(data['theEntry'][0]['Date_of_Effectivity']);
+                $('#Name_Suffix_ID').val(data['theEntry'][0]['Name_Suffix_ID']);
+                $('#Ordinance_Resolution_Title').val(data['theEntry'][0]['Ordinance_Resolution_Title']);
+                $('#Status_of_Ordinance_or_Resolution_ID').val(data['theEntry'][0]['Status_of_Ordinance_or_Resolution_ID']);
+                $('#Abstract_Content').val(data['theEntry'][0]['Abstract_Content']);
+                $('#Previous_Related_Ordinance_Resolution_ID').val(data['theEntry'][0]['Previous_Related_Ordinance_Resolution_ID']);
+                $('#Region_ID').val(data['theEntry'][0]['Region_ID']);
+
+                var barangay =
+                    " <option value='" + data['theEntry'][0]['Barangay_ID'] + "' selected>" + data['theEntry'][0]['Barangay_Name'] + "</option>";
+                $('#Barangay_ID').append(barangay);
+
+                var city =
+                    " <option value='" + data['theEntry'][0]['City_Municipality_ID'] + "' selected>" + data['theEntry'][0]['City_Municipality_Name'] + "</option>";
+                $('#City_Municipality_ID').append(city);
+
+                var province =
+                    " <option value='" + data['theEntry'][0]['Province_ID'] + "' selected>" + data['theEntry'][0]['Province_Name'] + "</option>";
+                $('#Province_ID').append(province);
+            }
+        });
+
+
+
+
+    });
+</script>
+
+<style>
+    table {
+        display: inline-block;
+        overflow-x: scroll;
+    }
+</style>
+
+@endsection
