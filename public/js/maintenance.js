a81037e47465fc5d65d17968e2322635aaea6b74
Ralf Pontilar <ralf.pontilar@yahoo.com>
2022-08-24 13:54:55 +0800

    Update BJISBH Maintenance
    Update BJISBH Maintenance

--- public/js/maintenance.js
+++ public/js/maintenance.js
@@ -1005,3 +1005,329 @@ $(document).on('click', '.updateThis_Case_Type', function (e) {
 });
 
 
+// Case
+//post buttons
+$(document).on('click', '.postThis_Case', function (e) {
+    $('#newBRGY_Case').submit();
+});
+
+$(document).on('click', ('.edit_case'), function (e) {
+
+    var disID = $(this).val();
+    $.ajax({
+        url: "/get_case_maint",
+        type: 'GET',
+        data: { id: disID },
+        fail: function () {
+            alert('request failed');
+        },
+        success: function (data) {
+            $('#this_case_idX').val(data['theEntry'][0]['Case_ID']);
+            $('#this_caseX').val(data['theEntry'][0]['Case_Name']);
+            $('#case_type_idX2').val(data['theEntry'][0]['Case_Type_ID']);
+            $('#this_case_active').empty();
+            $('#this_case_active').val(data['theEntry'][0]['Active']);
+            if (data['theEntry'][0]['Active'] == 1) {
+                $('#this_case_active').append('Yes');
+            } else {
+                $('#this_case_active').append('No');
+            }
+
+        }
+    });
+});
+
+$(document).on('click', '.updateThis_Case', function (e) {
+    $('#updateBRGY_Case').submit();
+});
+
+// Type of Involved Party
+//post buttons
+$(document).on('click', '.postThis_Type_of_Involved_Party', function (e) {
+    $('#newBRGY_Type_of_Involved_Party').submit();
+});
+
+$(document).on('click', ('.edit_type_of_involved_party'), function (e) {
+
+    var disID = $(this).val();
+    $.ajax({
+        url: "/get_type_of_involved_party_maint",
+        type: 'GET',
+        data: { id: disID },
+        fail: function () {
+            alert('request failed');
+        },
+        success: function (data) {
+            $('#this_type_of_involved_party_idX').val(data['theEntry'][0]['Type_of_Involved_Party_ID']);
+            $('#this_type_of_involved_partyX').val(data['theEntry'][0]['Type_of_Involved_Party']);
+
+            $('#this_type_of_involved_party_active').empty();
+            $('#this_type_of_involved_party_active').val(data['theEntry'][0]['Active']);
+            if (data['theEntry'][0]['Active'] == 1) {
+                $('#this_type_of_involved_party_active').append('Yes');
+            } else {
+                $('#this_type_of_involved_party_active').append('No');
+            }
+
+        }
+    });
+});
+
+$(document).on('click', '.updateThis_Type_of_Involved_Party', function (e) {
+    $('#updateBRGY_Type_of_Involved_Party').submit();
+});
+
+
+// Violation Status
+//post buttons
+$(document).on('click', '.postThis_Violation_Status', function (e) {
+    $('#newBRGY_Violation_Status').submit();
+});
+
+$(document).on('click', ('.edit_violation_status'), function (e) {
+
+    var disID = $(this).val();
+    $.ajax({
+        url: "/get_violation_status_maint",
+        type: 'GET',
+        data: { id: disID },
+        fail: function () {
+            alert('request failed');
+        },
+        success: function (data) {
+            $('#this_violation_status_idX').val(data['theEntry'][0]['Violation_Status_ID']);
+            $('#this_violation_statusX').val(data['theEntry'][0]['Violation_Status']);
+
+            $('#this_violation_status_active').empty();
+            $('#this_violation_status_active').val(data['theEntry'][0]['Active']);
+            if (data['theEntry'][0]['Active'] == 1) {
+                $('#this_violation_status_active').append('Yes');
+            } else {
+                $('#this_violation_status_active').append('No');
+            }
+
+        }
+    });
+});
+
+$(document).on('click', '.updateThis_Violation_Status', function (e) {
+    $('#updateBRGY_Violation_Status').submit();
+});
+
+// Summons Status
+//post buttons
+$(document).on('click', '.postThis_Summons_Status', function (e) {
+    $('#newBRGY_Summons_Status').submit();
+});
+
+$(document).on('click', ('.edit_summons_status'), function (e) {
+
+    var disID = $(this).val();
+    $.ajax({
+        url: "/get_summons_status_maint",
+        type: 'GET',
+        data: { id: disID },
+        fail: function () {
+            alert('request failed');
+        },
+        success: function (data) {
+            $('#this_summons_status_idX').val(data['theEntry'][0]['Summons_Status_ID']);
+            $('#this_summons_statusX').val(data['theEntry'][0]['Type_of_Action']);
+
+            $('#this_summons_status_active').empty();
+            $('#this_summons_status_active').val(data['theEntry'][0]['Active']);
+            if (data['theEntry'][0]['Active'] == 1) {
+                $('#this_summons_status_active').append('Yes');
+            } else {
+                $('#this_summons_status_active').append('No');
+            }
+
+        }
+    });
+});
+
+$(document).on('click', '.updateThis_Summons_Status', function (e) {
+    $('#updateBRGY_Summons_Status').submit();
+});
+
+// Service Rate
+//post buttons
+$(document).on('click', '.postThis_Service_Rate', function (e) {
+    $('#newBRGY_Service_Rate').submit();
+});
+
+$(document).on('click', ('.edit_service_rate'), function (e) {
+
+    var disID = $(this).val();
+    $.ajax({
+        url: "/get_service_rate_maint",
+        type: 'GET',
+        data: { id: disID },
+        fail: function () {
+            alert('request failed');
+        },
+        success: function (data) {
+            $('#this_service_rate_idX').val(data['theEntry'][0]['Service_Rate_ID']);
+            $('#this_service_rateX').val(data['theEntry'][0]['Service_Rate']);
+
+            $('#this_service_rate_active').empty();
+            $('#this_service_rate_active').val(data['theEntry'][0]['Active']);
+            if (data['theEntry'][0]['Active'] == 1) {
+                $('#this_service_rate_active').append('Yes');
+            } else {
+                $('#this_service_rate_active').append('No');
+            }
+
+        }
+    });
+});
+
+$(document).on('click', '.updateThis_Service_Rate', function (e) {
+    $('#updateBRGY_Service_Rate').submit();
+});
+
+// Proceedings Status
+//post buttons
+$(document).on('click', '.postThis_Proceedings_Status', function (e) {
+    $('#newBRGY_Proceedings_Status').submit();
+});
+
+$(document).on('click', ('.edit_proceedings_status'), function (e) {
+
+    var disID = $(this).val();
+    $.ajax({
+        url: "/get_proceedings_status_maint",
+        type: 'GET',
+        data: { id: disID },
+        fail: function () {
+            alert('request failed');
+        },
+        success: function (data) {
+            $('#this_proceedings_status_idX').val(data['theEntry'][0]['Proceedings_Status_ID']);
+            $('#this_proceedings_statusX').val(data['theEntry'][0]['Type_of_Action']);
+
+            $('#this_proceedings_status_active').empty();
+            $('#this_proceedings_status_active').val(data['theEntry'][0]['Active']);
+            if (data['theEntry'][0]['Active'] == 1) {
+                $('#this_proceedings_status_active').append('Yes');
+            } else {
+                $('#this_proceedings_status_active').append('No');
+            }
+
+        }
+    });
+});
+
+$(document).on('click', '.updateThis_Proceedings_Status', function (e) {
+    $('#updateBRGY_Proceedings_Status').submit();
+});
+
+// Type of Action
+//post buttons
+$(document).on('click', '.postThis_Type_of_Action', function (e) {
+    $('#newBRGY_Type_of_Action').submit();
+});
+
+$(document).on('click', ('.edit_type_of_action'), function (e) {
+
+    var disID = $(this).val();
+    $.ajax({
+        url: "/get_type_of_action_maint",
+        type: 'GET',
+        data: { id: disID },
+        fail: function () {
+            alert('request failed');
+        },
+        success: function (data) {
+            $('#this_type_of_action_idX').val(data['theEntry'][0]['Type_of_Action_ID']);
+            $('#this_type_of_actionX').val(data['theEntry'][0]['Type_of_Action']);
+
+            $('#this_type_of_action_active').empty();
+            $('#this_type_of_action_active').val(data['theEntry'][0]['Active']);
+            if (data['theEntry'][0]['Active'] == 1) {
+                $('#this_type_of_action_active').append('Yes');
+            } else {
+                $('#this_type_of_action_active').append('No');
+            }
+
+        }
+    });
+});
+
+$(document).on('click', '.updateThis_Type_of_Action', function (e) {
+    $('#updateBRGY_Type_of_Action').submit();
+});
+
+// Type of Penalties
+//post buttons
+$(document).on('click', '.postThis_Type_of_Penalties', function (e) {
+    $('#newBRGY_Type_of_Penalties').submit();
+});
+
+$(document).on('click', ('.edit_type_of_penalties'), function (e) {
+
+    var disID = $(this).val();
+    $.ajax({
+        url: "/get_type_of_penalties_maint",
+        type: 'GET',
+        data: { id: disID },
+        fail: function () {
+            alert('request failed');
+        },
+        success: function (data) {
+            $('#this_type_of_penalties_idX').val(data['theEntry'][0]['Types_of_Penalties_ID']);
+            $('#this_type_of_penaltiesX').val(data['theEntry'][0]['Type_of_Penalties']);
+
+            $('#this_type_of_penalties_active').empty();
+            $('#this_type_of_penalties_active').val(data['theEntry'][0]['Active']);
+            if (data['theEntry'][0]['Active'] == 1) {
+                $('#this_type_of_penalties_active').append('Yes');
+            } else {
+                $('#this_type_of_penalties_active').append('No');
+            }
+
+        }
+    });
+});
+
+$(document).on('click', '.updateThis_Type_of_Penalties', function (e) {
+    $('#updateBRGY_Type_of_Penalties').submit();
+});
+
+
+// Blotter Status
+//post buttons
+$(document).on('click', '.postThis_Blotter_Status', function (e) {
+    $('#newBRGY_Blotter_Status').submit();
+});
+
+$(document).on('click', ('.edit_blotter_status'), function (e) {
+
+    var disID = $(this).val();
+    $.ajax({
+        url: "/get_blotter_status_maint",
+        type: 'GET',
+        data: { id: disID },
+        fail: function () {
+            alert('request failed');
+        },
+        success: function (data) {
+            $('#this_blotter_status_idX').val(data['theEntry'][0]['Blotter_Status_ID']);
+            $('#this_blotter_statusX').val(data['theEntry'][0]['Blotter_Status_Name']);
+
+            $('#this_blotter_status_active').empty();
+            $('#this_blotter_status_active').val(data['theEntry'][0]['Active']);
+            if (data['theEntry'][0]['Active'] == 1) {
+                $('#this_blotter_status_active').append('Yes');
+            } else {
+                $('#this_blotter_status_active').append('No');
+            }
+
+        }
+    });
+});
+
+$(document).on('click', '.updateThis_Blotter_Status', function (e) {
+    $('#updateBRGY_Blotter_Status').submit();
+});
+
---
+++ resources/views/maintenance/bjisbh_summons_status.blade.php
@@ -0,0 +1,132 @@
+@extends('layouts.default')
+
+@section('content')
+<script src="{{ asset('/js/maintenance.js') }}" defer></script>
+<link href="{{ asset('/css/maintenance.css') }}" rel="stylesheet">
+
+<div class="page_title_row col-md-12">
+    <div class="col-md-6 titleXZ"> Summons Status Maintenance/Setup </div>
+    <div class="col-md-6 breadcrumbXZ"> 
+        <ol class="breadcrumb">
+            <a href="{{route('home')}}"><li>DILG_BMS / </li></a>
+            <li> &nbsp;Summons Status</li>
+        </ol> 
+    </div>
+</div>
+<div class="tableX_row col-md-12 up_marg5">
+    <div class="flexer"> 
+        <div class="eighty_split">{{$db_entries->appends(['db_entries' => $db_entries->currentPage()])->links()}}</div>
+        <div class="twenty_split txtRight"><button data-toggle="modal" data-target="#createSummons_Status">New</button></div>
+    </div>
+    <div class="col-md-12">
+        <table class="table-bordered table_gen up_marg5">
+            <thead>
+                <tr>
+                    <th>Summons_Status_ID </th>
+                    <th>Type_of_Action</th>
+                    <th>Active</th>
+                    <th>Encoder_ID</th>
+                    <th>Date_Stamp</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($db_entries as $x)
+                    <tr>
+                        <td class="sm_data_col txtCtr">{{$x->Summons_Status_ID}}</td>
+                        <td>{{$x->Type_of_Action}}</td>
+                        <td class="sm_data_col txtCtr">{{$x->Active}}</td>
+                        <td class="sm_data_col txtCtr">{{$x->Encoder_ID}}</td>
+                        <td class="md_data_col txtCtr">{{$x->Date_Stamp}}</td>
+                        <td class="sm_data_col txtCtr">
+                            <button class="edit_summons_status" value="{{$x->Summons_Status_ID}}" data-toggle="modal" data-target="#updateSummons_Status">Edit</button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<!-- Create Summons_Status Modal -->
+<div class="modal fade" id="createSummons_Status" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title flexer justifier">Create Post</h4>
+        </div>
+        <form id="newBRGY_Summons_Status" method="POST" action="{{ route('create_summons_status_maint') }}" autocomplete="off" enctype="multipart/form-data">@csrf
+            <div class="modal-body Absolute-Center">
+                <div class="modal_input_container">
+                    <div class="up_marg5">
+                        <span><b>Summons_Status:</b></span><br>
+                        <input class="modal_input1" name="Type_of_ActionX">
+                    </div>
+
+                    <div class="up_marg5">
+                        <span><b>Active:</b></span><br>
+                        <select class="modal_input1" name="ActiveX">
+                            <option value=1 hidden selected>Is Active?</option>
+                            <option value=1>Yes</option>
+                            <option value=0>No</option>
+                        </select>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn postThis_Summons_Status modal_sb_button">Create</button>
+            </div>
+        </form>
+      </div>
+      
+    </div>
+</div>
+
+<!-- Create Summons_Status END -->
+
+<!-- Edit/Update Summons_Status Modal -->
+<div class="modal fade" id="updateSummons_Status" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title flexer justifier">Create Post</h4>
+        </div>
+        <form id="updateBRGY_Summons_Status" method="POST" action="{{ route('update_summons_status_maint') }}" autocomplete="off" enctype="multipart/form-data">@csrf
+            <div class="modal-body Absolute-Center">
+                <div class="modal_input_container">
+                    <div class="up_marg5">
+                        <span><b>Summons_Status:</b></span><br>
+                        <input id="this_summons_status_idX" class="modal_input1" name="Summons_Status_idX" hidden>
+                        <input id="this_summons_statusX" class="modal_input1" name="Type_of_ActionX2">
+                    </div>
+
+                    <div class="up_marg5">
+                        <span><b>Active:</b></span><br>
+                        <select class="modal_input1" name="ActiveX2">
+                            <option id="this_summons_status_active" value=1 hidden selected>Is Active?</option>
+                            <option value=1>Yes</option>
+                            <option value=0>No</option>
+                        </select>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn updateThis_Summons_Status modal_sb_button">Save</button>
+            </div>
+        </form>
+      </div>
+      
+    </div>
+</div>
+
+<!-- Edit/Update Summons_Status END -->
+
+@endsection
