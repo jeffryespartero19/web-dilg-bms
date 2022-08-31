b77ae81cddb876bad92b55642d21bc744676fb5d
Ralf Pontilar <ralf.pontilar@yahoo.com>
2022-08-31 08:50:11 +0800

    UPdate Login

--- app/Http/Controllers/BRGYLoginController.php
+++ app/Http/Controllers/BRGYLoginController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class BRGYLoginController extends Controller
 {
     public function index($b_id)
     {
         $Barangay_ID = $b_id;
-        
-        return view('auth.login', compact('Barangay_ID'));
+        $barangay = DB::table('maintenance_barangay')->where('Barangay_ID', $Barangay_ID)->get();
+
+
+        return view('auth.login', compact('Barangay_ID', 'barangay'));
     }
 }
