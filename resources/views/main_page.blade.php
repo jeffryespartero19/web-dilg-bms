f8c088d4828aeff8a9672c252b4ae1dad87add29
Ralf <ralf.pontilar@yahoo.com>
2022-10-11 09:41:28 +0800

    Update Mainpage

--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -3,254 +3,116 @@
 
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>DILG_BIS</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <link rel="shortcut icon" href="{{ asset('css/logos/DILG_logo.png') }}">
-
-    <!-- Styles -->
-    <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links>a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
-    <!-- Scripts -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="{{ asset('js/global.js') }}" defer></script>
-
-    <script type="text/javascript">
-        (function() {
-            var css = document.createElement('link');
-            css.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
-            css.rel = 'stylesheet';
-            css.type = 'text/css';
-            document.getElementsByTagName('head')[0].appendChild(css);
-        })();
-    </script>
-
-    <script>
-        var msg = '{{Session::get('
-        alert ')}}';
-        var exist = '{{Session::has('
-        alert ')}}';
-        if (exist) {
-            alert(msg);
-        }
-    </script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet'>
-    <link href='https://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet' type='text/css'>
-
-    <!-- Styles -->
-    <link rel="shortcut icon" href="{{ asset('css/logos/DILG_logo.png') }}">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
-    <link href="{{ asset('css/global.css') }}" rel="stylesheet">
-
-    <script src="{{ asset('/js/homeX.js') }}" defer></script>
-    <link href="{{ asset('/css/homeX.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/general.css') }}" rel="stylesheet">
+    @include('includes.head')
 </head>
 
-<body class="">
-    <div id="main_public">
-        <div class="top-right links" style="z-index: 999">
+<body class="hold-transition layout-top-nav">
+    <div class="wrapper">
 
-            <a href="" class="text_hoverable">FAQ</a>
-            <a href="{{ route('login') }}" class="text_hoverable">Login</a>
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand-md navbar-light navbar-dark">
+            <div class="container">
+                <a href="../../index3.html" class="navbar-brand">
+                    <span class="brand-text font-weight-light"><b>DILG</b> Barangay Management System</span>
+                </a>
 
-            @if (Route::has('register'))
-            <a href="{{ url('registers') }}" class="text_hoverable">Register</a>
-            @endif
+                <!-- Right navbar links -->
+                <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
+                    <!-- Messages Dropdown Menu -->
+                    <li class="nav-item">
+                        <a href="{{ route('login') }}" class="nav-link">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ url('registers') }}" class="nav-link">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- /.navbar -->
 
-        </div>
-        <div class="theContent">
-            <div class=" justify-content-center" style="width: 100%;">
-                <div class="col-md-12 topBanner">
-                    <div class="bannerTitle">
-                        <span class="intro_txt txtHide firstln">Barangay</span><br>
-                        <span class="intro_txt txtHide secondln">Online Web Services</span><br>
-                        <span class="intro_txt txtHide thirdln"></span><br>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content" style="height: 150px;">
+                <div class="container">
+                    <div class="row " style="padding-top: 20px;">
+                        <img src="{{ asset('/dist/img/logo-dilg-new.png') }}">
                     </div>
                 </div>
+            </div>
+            <!-- /.content-header -->
 
-                <div class="col-md-9 middlePane">
-                    <div class="flexer justifier">
-
-                    </div>
-                    <div class="flexer justifier">
-
-                    </div>
-                </div>
-                <div class="col-md-3 rightPane">
-                    <div>
-                        <form id="BRGYFilter" action="{{ route('main') }}">
-                            @csrf
-                            <h2>Filters</h2>
-                            <br>
-                            <div class="row">
-                                <div class="up_marg5">
-                                    <span><b>Region:</b></span><br>
-                                    <select class="modal_input1 regionX" name="ActiveX">
-                                        <option value='' hidden selected>Select</option>
-                                        @foreach($regionX as $rx)
-                                            <option value='{{$rx->Region_ID}}' >{{$rx->Region_Name}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="up_marg5">
-                                    <span><b>Province:</b></span><br>
-                                    <select class="modal_input1 provX" name="ActiveX">
-                                        <option value='' hidden selected>Select</option>
-                                    </select>
-                                </div>
-                                <div class="up_marg5">
-                                    <span><b>City/Municipality:</b></span><br>
-                                    <select class="modal_input1 cityX" name="ActiveX">
-                                        <option value='' hidden selected>Select</option>
-                                    </select>
-                                </div>
-                                <div class="up_marg5">
-                                    <span><b>Barangay:</b></span><br>
-                                    <select class="modal_input1 brgyX" name="ActiveX">
-                                        <option value='' hidden selected>Select</option>
-                                    </select>
-                                </div>
-                                <div class="form-group" style="padding:0 10px; text-align:right">
-                                    <button type="submit" id="b_id" value="0">Go</button>
-                                </div>
+            <!-- Main content -->
+            <div class="content">
+                <div class="row bg-warning" style="height: 250px; padding-top:100px">
+                    <div class="col-12">
+                        <center>
+                            <div class="input-group" style="width: 50%;">
+                                <input class="form-control" id="search" type="search" placeholder="Search Barangay" aria-label="Search" style="font-size: 40px; height:60px; width:600px">
                             </div>
-                        </form>
+                        </center>
                     </div>
                 </div>
+                <div class="row bg-secondary" style="height: 50px; margin-bottom:50px">
+
+                </div>
+                <div class="container">
+                    <h1>hello</h1>
+                </div>
             </div>
+            <!-- /.content -->
         </div>
-    </div>
-    {{-- <footer class="">
-            <div>
-                <span style="font-size: 11px;"> Wizzard Technologies, Inc. DILG-BIS</span>
+        <!-- /.content-wrapper -->
+
+
+        <!-- Main Footer -->
+        <footer class="main-footer">
+            <!-- To the right -->
+            <div class="float-right d-none d-sm-inline">
+                Anything you want
             </div>
-        </footer> --}}
+            <!-- Default to the left -->
+            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        </footer>
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+
+    <!-- jQuery -->
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../../dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../../dist/js/demo.js"></script>
 
     <script>
-        //list province
-        $(document).on('change','.regionX',function(e) {
-            var disID = $(this).find(":selected").val();
+        $("#search").keyup(function() {
+
+            var text = $(this).val();
+            alert(text);
             $.ajax({
-                url: "/list_province",
-                type: 'GET',
-                data: { id: disID },
-                fail: function(){
-                    alert('request failed');
+                type: "GET",
+                url: "/search_barangay/" + text,
+                fail: function() {
+                    alert("request failed");
                 },
-                success: function (data) { 
-                    $('.provX').empty();
-                    $('.provX').append('<option value="" selected> Select </option>');
-                    $.each(data['provinceX'], function(index, value) {
-                        $('.provX').append('<option value="' + data['provinceX'][index]['Province_ID'] + '">' + data['provinceX'][index]['Province_Name']+ '</option>');
+                success: function(data) {
+                    var data = JSON.parse(data);
+                    $('#Province_ID').empty();
+                    $('#City_Municipality_ID').empty();
+                    $('#Barangay_ID').empty();
+
+                    data.forEach(element => {
+                        var option = " <option value='" +
+                            element["Province_ID"] +
+                            "'>" +
+                            element["Province_Name"] +
+                            "</option>";
+                        $('#Province_ID').append(option);
                     });
-
-                }
-            });
-        });
-
-        //list province
-        $(document).on('change','.provX',function(e) {
-            var disID = $(this).find(":selected").val();
-            $.ajax({
-                url: "/list_city",
-                type: 'GET',
-                data: { id: disID },
-                fail: function(){
-                    alert('request failed');
-                },
-                success: function (data) { 
-                    $('.cityX').empty();
-                    $('.cityX').append('<option value="" selected> Select </option>');
-                    $.each(data['cityX'], function(index, value) {
-                        $('.cityX').append('<option value="' + data['cityX'][index]['City_Municipality_ID'] + '">' + data['cityX'][index]['City_Municipality_Name']+ '</option>');
-                    });
-
-                }
-            });
-        });
-
-        //list brgy
-        $(document).on('change','.cityX',function(e) {
-            var disID = $(this).find(":selected").val();
-            $.ajax({
-                url: "/list_brgy",
-                type: 'GET',
-                data: { id: disID },
-                fail: function(){
-                    alert('request failed');
-                },
-                success: function (data) { 
-                    
-                    $('.brgyX').empty();
-                    $('.brgyX').append('<option value="" selected> Select </option>');
-                    $.each(data['brgyX'], function(index, value) {
-                        $('.brgyX').append('<option value="' + data['brgyX'][index]['Barangay_ID'] + '">' + data['brgyX'][index]['Barangay_Name']+ '</option>');
-                    });
-
                 }
             });
         });
