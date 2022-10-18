48d473a48efffb5a49ae3a9093c02def1aae966b
Ralf Pontilar <ralf.pontilar@yahoo.com>
2022-10-19 05:03:38 +0800

    Update BIPS BFAS Maintenance

--- resources/views/maintenance/bips_Civil_Status.blade.php
+++ resources/views/maintenance/bips_Civil_Status.blade.php
@@ -5,84 +5,130 @@
 <link href="{{ asset('/css/maintenance.css') }}" rel="stylesheet">
 
 <div class="page_title_row col-md-12">
-    <div class="col-md-6 titleXZ"> Civil Status Maintenance/Setup </div>
-    <div class="col-md-6 breadcrumbXZ"> 
-        <ol class="breadcrumb">
-            <a href="{{route('home')}}"><li>DILG_BMS / </li></a>
-            <li> &nbsp;civil status</li>
-        </ol> 
-    </div>
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Civil Status Maintenance/Setup</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{route('home')}}">DILG_BMS</a></li>
+                        <li class="breadcrumb-item active">Civil Status Maintenance/Setup</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+    </section>
 </div>
-<div class="tableX_row col-md-12 up_marg5">
-    <div class="flexer"> 
-        <div class="eighty_split">{{$db_entries->appends(['db_entries' => $db_entries->currentPage()])->links()}}</div>
-        <div class="twenty_split txtRight"><button data-toggle="modal" data-target="#createCivil_Status">New</button></div>
-    </div>
-    <div class="col-md-12">
-        <table class="table-bordered table_gen up_marg5">
-            <thead>
-                <tr>
-                    <th>Civil_Status_ID </th>
-                    <th>Civil_Status</th>
-                    <th>Active</th>
-                    <th>Encoder_ID</th>
-                    <th>Date_Stamp</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($db_entries as $x)
-                    <tr>
-                        <td class="sm_data_col txtCtr">{{$x->Civil_Status_ID}}</td>
-                        <td>{{$x->Civil_Status}}</td>
-                        <td class="sm_data_col txtCtr">{{$x->Active}}</td>
-                        <td class="sm_data_col txtCtr">{{$x->Encoder_ID}}</td>
-                        <td class="md_data_col txtCtr">{{$x->Date_Stamp}}</td>
-                        <td class="sm_data_col txtCtr">
-                            <button class="edit_civil_status" value="{{$x->Civil_Status_ID}}" data-toggle="modal" data-target="#updateCivil_Status">Edit</button>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
 </div>
+@endif
+@if(session()->has('message'))
+<div class="alert alert-success">
+    {{ session()->get('message') }}
+</div>
+@endif
+
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div style="text-align: right;">
+                            <div class="btn-group">
+                                <div style="padding: 2px;"><button data-toggle="modal" class="btn btn-success" data-target="#createCivil_Status" style="width: 100px;">New</button></div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="tableX_row col-md-12 up_marg5">
+                            <div class="table-responsive">
+                                <table id="example" class="table table-striped table-bordered" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th>Civil_Status_ID </th>
+                                            <th>Civil_Status</th>
+                                            <th>Active</th>
+                                            <th>Encoder_ID</th>
+                                            <th>Date_Stamp</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($db_entries as $x)
+                                        <tr>
+                                            <td class="sm_data_col txtCtr">{{$x->Civil_Status_ID}}</td>
+                                            <td>{{$x->Civil_Status}}</td>
+                                            <td class="sm_data_col txtCtr">{{$x->Active}}</td>
+                                            <td class="sm_data_col txtCtr">{{$x->Encoder_ID}}</td>
+                                            <td class="md_data_col txtCtr">{{$x->Date_Stamp}}</td>
+                                            <td class="sm_data_col txtCtr">
+                                                <button class="edit_civil_status" value="{{$x->Civil_Status_ID}}" data-toggle="modal" data-target="#updateCivil_Status">Edit</button>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</section>
+<!-- /.content -->
 
 <!-- Create Civil_Status Modal -->
 <div class="modal fade" id="createCivil_Status" role="dialog">
     <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title flexer justifier">Create Post</h4>
-        </div>
-        <form id="newBRGY_Civil_Status" method="POST" action="{{ route('create_civil_status_maint') }}" autocomplete="off" enctype="multipart/form-data">@csrf
-            <div class="modal-body Absolute-Center">
-                <div class="modal_input_container">
-                    <div class="up_marg5">
-                        <span><b>Civil_Status:</b></span><br>
-                        <input class="modal_input1" name="Civil_StatusX">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title flexer justifier">Create New Entry</h4>
+                <button type="button" class="close modal-close" data-dismiss="modal">&times;</button>
+            </div>
+            <form id="newBRGY_Civil_Status" method="POST" action="{{ route('create_civil_status_maint') }}" autocomplete="off" enctype="multipart/form-data">@csrf
+                <div class="modal-body Absolute-Center">
+                    <div class="modal_input_container">
+                        <div class="form-group">
+                            <label>Civil_Status:</label>
+                            <input class="form-control" name="Civil_StatusX">
+                        </div>
+
+                        <div class="form-group">
+                            <label>Active:</label>
+                            <select class="form-control" name="ActiveX">
+                                <option value=1 hidden selected>Is Active?</option>
+                                <option value=1>Yes</option>
+                                <option value=0>No</option>
+                            </select>
+                        </div>
                     </div>
 
-                    <div class="up_marg5">
-                        <span><b>Active:</b></span><br>
-                        <select class="modal_input1" name="ActiveX">
-                            <option value=1 hidden selected>Is Active?</option>
-                            <option value=1>Yes</option>
-                            <option value=0>No</option>
-                        </select>
-                    </div>
                 </div>
-                
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn postThis_Civil_Status modal_sb_button">Create</button>
-            </div>
-        </form>
-      </div>
-      
+                <div class="modal-footer">
+                    <button type="button" class="btn postThis_Civil_Status modal_sb_button">Create</button>
+                </div>
+            </form>
+        </div>
+
     </div>
 </div>
 
@@ -91,39 +137,39 @@
 <!-- Edit/Update Civil_Status Modal -->
 <div class="modal fade" id="updateCivil_Status" role="dialog">
     <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title flexer justifier">Create Post</h4>
-        </div>
-        <form id="updateBRGY_Civil_Status" method="POST" action="{{ route('update_civil_status_maint') }}" autocomplete="off" enctype="multipart/form-data">@csrf
-            <div class="modal-body Absolute-Center">
-                <div class="modal_input_container">
-                    <div class="up_marg5">
-                        <span><b>Civil_Status:</b></span><br>
-                        <input id="this_civil_status_idX" class="modal_input1" name="Civil_Status_idX" hidden>
-                        <input id="this_civil_statusX" class="modal_input1" name="Civil_StatusX2">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title flexer justifier">Update Entry</h4>
+                <button type="button" class="close modal-close" data-dismiss="modal">&times;</button>
+            </div>
+            <form id="updateBRGY_Civil_Status" method="POST" action="{{ route('update_civil_status_maint') }}" autocomplete="off" enctype="multipart/form-data">@csrf
+                <div class="modal-body Absolute-Center">
+                    <div class="modal_input_container">
+                        <div class="form-group">
+                            <label>Civil_Status:</label>
+                            <input id="this_civil_status_idX" class="form-control" name="Civil_Status_idX" hidden>
+                            <input id="this_civil_statusX" class="form-control" name="Civil_StatusX2">
+                        </div>
+
+                        <div class="form-group">
+                            <label>Active:</label>
+                            <select class="form-control" name="ActiveX2">
+                                <option id="this_civil_status_active" value=1 hidden selected>Is Active?</option>
+                                <option value=1>Yes</option>
+                                <option value=0>No</option>
+                            </select>
+                        </div>
                     </div>
 
-                    <div class="up_marg5">
-                        <span><b>Active:</b></span><br>
-                        <select class="modal_input1" name="ActiveX2">
-                            <option id="this_civil_status_active" value=1 hidden selected>Is Active?</option>
-                            <option value=1>Yes</option>
-                            <option value=0>No</option>
-                        </select>
-                    </div>
                 </div>
-                
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn updateThis_Civil_Status modal_sb_button">Save</button>
-            </div>
-        </form>
-      </div>
-      
+                <div class="modal-footer">
+                    <button type="button" class="btn updateThis_Civil_Status modal_sb_button">Save</button>
+                </div>
+            </form>
+        </div>
+
     </div>
 </div>
 
