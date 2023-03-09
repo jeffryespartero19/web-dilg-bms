0f46feb33b02ce303a3de7ada497469631bbbafa
Marc Alo <apcmgalo48@gmail.com>
2023-03-09 10:35:03 +0800

    BMS fix PSGC search

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -82,29 +82,26 @@ class GlobalController extends Controller
     {
         //$text='Morong';
         $cityX = DB::table('maintenance_city_municipality')->where('City_Municipality_Name', 'LIKE', '%' . $text . '%')->get();
-
+//dd($cityX);
         $cityPSGC = $cityX->pluck('PSGC_code');
         $cityIDs = $cityX->pluck('City_Municipality_ID');
         $provIDs = $cityX->pluck('Province_ID');
         $regionIDs = $cityX->pluck('Region_ID');
 
-        $provPSGC = DB::table('maintenance_province')->whereIn('Province_ID', $provIDs)->pluck('PSGC_code');
-        $regionPSGC = DB::table('maintenance_region')->whereIn('Region_ID', $regionIDs)->pluck('PSGC_code');
-
-
-
-        // dd($cityPSGC,$provIDs,$regionPSGC,$cityIDs);
         $countX = count($cityIDs);
         $data = [];
-
+//dd($cityIDs,$provIDs,$regionIDs);
         for ($i = 0; $i < $countX; $i++) {
+            $provPSGC = DB::table('maintenance_province')->where('Province_ID', $provIDs[$i])->pluck('PSGC_code');
+            $regionPSGC = DB::table('maintenance_region')->where('Region_ID', $regionIDs[$i])->pluck('PSGC_code');
+
             $dataX = DB::table('maintenance_barangay as a')
                 ->leftjoin('maintenance_city_municipality as b', 'b.City_Municipality_ID', '=', DB::raw($cityIDs[$i]))
                 ->leftjoin('maintenance_province as c', 'c.Province_ID', '=', DB::raw($provIDs[$i]))
                 ->leftjoin('maintenance_region as d', 'd.Region_ID', '=', DB::raw($regionIDs[$i]))
                 ->select('a.Barangay_Name', 'b.City_Municipality_Name', 'c.Province_Name', 'd.Region_Name', 'a.Barangay_ID')
-                ->where('a.Region_ID', $regionPSGC[$i])
-                ->where('a.Province_ID', $provPSGC[$i])
+                ->where('a.Region_ID', $regionPSGC)
+                ->where('a.Province_ID', $provPSGC)
                 ->where('a.City_Municipality_ID', $cityPSGC[$i])
                 ->get();
 
@@ -118,7 +115,7 @@ class GlobalController extends Controller
         // dd($cityIDs);
 
 
-        //dd($data);
+       // dd($data);
         return json_encode($data);
     }
 
